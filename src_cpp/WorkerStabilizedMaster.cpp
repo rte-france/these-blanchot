fd596674daa1a65a25c84a5d1f6609b585fde531
Manuel Ruiz <manuel.ruiz@rte-france.com>
2018-05-10 23:05:24 +0200

    --

---
+++ src_cpp/WorkerStabilizedMaster.cpp
@@ -0,0 +1,13 @@
+#include "WorkerStabilizedMaster.h"
+
+
+
+WorkerStabilizedMaster::WorkerStabilizedMaster():WorkerMaster() {
+
+}
+WorkerStabilizedMaster::WorkerStabilizedMaster(std::string const & problem_name, int nslaves ) : WorkerMaster(problem_name, nslaves) {
+
+}
+WorkerStabilizedMaster::~WorkerStabilizedMaster() {
+
+}
