3f54fe20083dff90237ea0aef1c3b662794c9ae8
Xavier Blanchot <xav.blanchot@gmail.com>
2020-11-18 15:54:47 +0100

    --

---
+++ src/src_cpp/exe/test_solvers_library/test.cpp
@@ -0,0 +1,13 @@
+//#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN
+#pragma once
+#include "catch2.hpp"
+
+#include "define_datas.hpp"
+/*
+int main(int argc, char *argv[]){
+
+    Catch::Session session; // There must be exactly one instance
+
+    return session.run();
+}*/
