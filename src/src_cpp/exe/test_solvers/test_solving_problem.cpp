a7d36d641d70356f9395a25dbfa114f80bc27f01
Xavier Blanchot <xav.blanchot@gmail.com>
2020-11-20 11:54:14 +0100

    --

--- src/src_cpp/exe/test_solvers/test_solving_problem.cpp
+++ src/src_cpp/exe/test_solvers/test_solving_problem.cpp
@@ -7,18 +7,21 @@
 #include "define_datas.hpp"
 
 
-TEST_CASE("3. A problem is solved and we can get the optimal solution", "[solve]") {
+TEST_CASE("3. A problem is solved and we can get the optimal solution with screen output", "[solve][.][output]") {
 
     AllDatas datas;
     fill_datas(datas);
 
     SolverFactory factory;
 
-    auto inst = GENERATE(MIP_TOY, MULTIKP, UNBD_PRB, INFEAS_PRB);
+    std::ofstream out("out.txt");
+
+    auto inst = GENERATE(MIP_TOY, MULTIKP);
     SECTION("Loop on the instances") {
 
         for (auto const& solver_name : factory.get_solvers_list()) {
 
+            std::cout << "Solver: " << solver_name << std::endl;
             std::string instance = datas[inst]._path;
             //========================================================================================
             // 1. declaration d'un objet solveur
@@ -27,127 +30,121 @@ TEST_CASE("3. A problem is solved and we can get the optimal solution", "[solve]
             //========================================================================================
             // 2. initialisation d'un probleme et lecture
             solver->init();
-            solver->add_stream(std::cout);
+            
 
             const std::string flags = "MPS";
             solver->read_prob(instance.c_str(), flags.c_str());
 
             //========================================================================================
             // 3. Solving with output on screen (redirected in a file)
-            SECTION("Solving the problem with solver's output solves it and the log is sent to a file out.txt") {
 
-                std::ofstream out("out.txt");
-                std::streambuf* coutbuf = std::cout.rdbuf(); //save old buf
-                std::cout.rdbuf(out.rdbuf()); //redirect std::cout to out.txt!
+            
+            solver->add_stream(std::cout);
+            solver->set_output_log_level(3);
+            int slv_status(0);
+            solver->solve_mip(slv_status);
 
-                solver->set_output_log_level(3);
-                int slv_status(0);
-                solver->solve_mip(slv_status);
-
-                
-
-                bool success = false;
-                for (auto stat : datas[inst]._status_int) {
-                    if (stat == slv_status) {
-                        SUCCEED();
-                        success = true;
-                        break;
-                    }
-                }
-                if (!success) {
-                    FAIL();
-                }
-
-                success = false;
-                for (auto stat : datas[inst]._status) {
-                    if (stat == solver->SOLVER_STRING_STATUS[slv_status]) {
-                        SUCCEED();
-                        success = true;
-                        break;
-                    }
-                }
-                if (!success) {
-                    FAIL();
-                }
-                if (solver->SOLVER_STRING_STATUS[slv_status] == "OPTIMAL")
-                {
-                    double mip_val(0);
-                    solver->get_mip_value(mip_val);
-                    REQUIRE(mip_val == datas[inst]._optval);
-                }
-
-                solver->free();
-                REQUIRE(solver->get_number_of_instances() == 1);
-
-                // Reset standard cout
-                std::cout << "DERNIER MESSAGE AVANT DE PASSER EN MODE SCREEN" << std::endl;
-                std::cout.rdbuf(coutbuf);
-                std::cout << "DERNIER MESSAGE AVANT DE PASSER EN MODE SCREEN" << std::endl;
-                // Check if a file has been written
-                std::ifstream std_solv_output("out.txt");
-                std::string line;
-                int cnt = 0;
-                while (getline(std_solv_output, line)) //Tant qu'on n'est pas � la fin, on lit
-                {
-                    cnt += 1;
+            bool success = false;
+            for (auto stat : datas[inst]._status_int) {
+                if (stat == slv_status) {
+                    SUCCEED();
+                    success = true;
                     break;
                 }
-                std::cout << "DERNIER MESSAGE AVANT DE PASSER EN MODE SCREEN" << std::endl;
-                REQUIRE(cnt > 0);
-
-                std_solv_output.close();
-                
-                std::exit(1);
+            }
+            if (!success) {
+                FAIL();
             }
 
-            //========================================================================================
-            // 4. Solving without output on screen
-            SECTION("Solving the problem without solver's output solves it and nothing is written in the output file out.txt") {
-                
-                solver->init();
-                solver->add_stream(std::cout);
-                REQUIRE(solver->get_number_of_instances() == 1);
-
-                solver->read_prob(instance.c_str(), flags.c_str());
-
-                solver->set_output_log_level(0);
-                int slv_status = 0;
-                solver->solve_mip(slv_status);
-
-                bool success = false;
-                for (auto stat : datas[inst]._status_int) {
-                    if (stat == slv_status) {
-                        SUCCEED();
-                        success = true;
-                        break;
-                    }
+            success = false;
+            for (auto stat : datas[inst]._status) {
+                if (stat == solver->SOLVER_STRING_STATUS[slv_status]) {
+                    SUCCEED();
+                    success = true;
+                    break;
                 }
-                if (!success) {
-                    FAIL();
-                }
-
-                success = false;
-                for (auto stat : datas[inst]._status) {
-                    if (stat == solver->SOLVER_STRING_STATUS[slv_status]) {
-                        SUCCEED();
-                        success = true;
-                        break;
-                    }
-                }
-                if (!success) {
-                    FAIL();
-                }
-
-                if (solver->SOLVER_STRING_STATUS[slv_status] == "OPTIMAL")
-                {
-                    double mip_val(0);
-                    solver->get_mip_value(mip_val);
-                    REQUIRE(mip_val == datas[inst]._optval);
-                }
-
-                solver->free();
-                REQUIRE(solver->get_number_of_instances() == 1);
             }
+            if (!success) {
+                FAIL();
+            }
+            if (solver->SOLVER_STRING_STATUS[slv_status] == "OPTIMAL")
+            {
+                double mip_val(0);
+                solver->get_mip_value(mip_val);
+                REQUIRE(mip_val == datas[inst]._optval);
+            }
+
+            solver->free();
+            REQUIRE(solver->get_number_of_instances() == 1);
         }
     }
+}
+
+//========================================================================================
+// 4. Solving without output on screen
+TEST_CASE("3. A problem is solved and we can get the optimal solution without output", "[solve][no-output]") {
+
+    AllDatas datas;
+    fill_datas(datas);
+
+    SolverFactory factory;
+
+    auto inst = GENERATE(MIP_TOY, MULTIKP, UNBD_PRB, INFEAS_PRB);
+    SECTION("Solving the problem without solver's output solves it and nothing is written in the output file out.txt") {
+
+        for (auto const& solver_name : factory.get_solvers_list()) {
+
+            std::string instance = datas[inst]._path;
+
+            //================================================================================
+            // Initialization
+            SolverAbstract::Ptr solver = factory.create_solver(solver_name);
+            solver->init();
+            solver->add_stream(std::cout);
+
+            const std::string flags = "MPS";
+            solver->read_prob(instance.c_str(), flags.c_str());
+
+            //================================================================================
+            // Desactivate output and solve
+            solver->set_output_log_level(0);
+            int slv_status = 0;
+            solver->solve_mip(slv_status);
+
+            bool success = false;
+            for (auto stat : datas[inst]._status_int) {
+                if (stat == slv_status) {
+                    SUCCEED();
+                    success = true;
+                    break;
+                }
+            }
+            if (!success) {
+                FAIL();
+            }
+
+            success = false;
+            for (auto stat : datas[inst]._status) {
+                if (stat == solver->SOLVER_STRING_STATUS[slv_status]) {
+                    SUCCEED();
+                    success = true;
+                    break;
+                }
+            }
+            if (!success) {
+                FAIL();
+            }
+
+            if (solver->SOLVER_STRING_STATUS[slv_status] == "OPTIMAL")
+            {
+                double mip_val(0);
+                solver->get_mip_value(mip_val);
+                REQUIRE(mip_val == datas[inst]._optval);
+            }
+
+            solver->free();
+            REQUIRE(solver->get_number_of_instances() == 1);
+        }
+    }
+
 }
